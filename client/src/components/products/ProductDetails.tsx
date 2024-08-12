440682f5d45abf840586b42105669951af605a62
Zakharii-Husar <zakhariihusar@gmail.com>
2024-08-11 23:17:19 -0500

    Added mock data for product details

--- client/src/components/products/ProductDetails.tsx
+++ client/src/components/products/ProductDetails.tsx
@@ -4,15 +4,25 @@ import { GET_PRODUCT_BY_ID } from "../../APIEndpoints";
 
 import type { paths, components } from "../../types/types";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 type GetProductResponse200 = paths["/api/products/{product_id}"]["get"]["responses"]["200"]
 type GetProductResponse404 = paths["/api/products/{product_id}"]["get"]["responses"]["404"]
 
 type ProductDetailsType = components["schemas"]["ProductDetailsResponse"];
 type NotFoundError = components["schemas"]["NotFoundError"];
+type InternalServerError = components["schemas"]["InternalServerError"];
 
-const fetchProductById = async (id: string): Promise<ProductDetailsType | NotFoundError> => {
+type ApiError = NotFoundError | InternalServerError | null;
+
+interface ApiResponse<T> {
+  status: number;
+  data: T | null;
+  error: ApiError;
+}
+
+
+const fetchProductById = async (id: string): Promise<ApiResponse<ProductDetailsType>> => {
   const link = GET_PRODUCT_BY_ID(id);
 
   const response = await fetch(link, {
@@ -23,34 +33,59 @@ const fetchProductById = async (id: string): Promise<ProductDetailsType | NotFou
   });
 
 
-  if (response.status == 200) {
-    const data: ProductDetailsType = await response.json();
-    return data;
+  if (response.status === 200) {
+    const result: ProductDetailsType = await response.json();
+    return { status: 200, data: result, error: null };
   }
-  if (response.status == 404) {
-    const data: NotFoundError = await response.json();
-    return data;
+  if (response.status === 404) {
+    const result: NotFoundError = await response.json();
+    return { status: 404, data: null, error: result };
   }
-  
-  return await response.json();
 
+  if (response.status === 500) {
+    const result: InternalServerError = await response.json();
+    return { status: 500, data: null, error: result };
+  }
+
+  return await response.json();
 }
 
 let productDetailsType: ProductDetailsType;
 
 const ProductDetails: React.FC = () => {
 
-  const [error, setError] = useState<null | NotFoundError>(null);
+  const [error, setError] = useState<ApiError>(null);
   const [loading, setLoading] = useState(false);
+  const [productDetails, setProductDetails] = useState<ProductDetailsType | null>(null);
+
+  useEffect(() => {
+    const getData = async () => {
+      setLoading(true);
+      const result = await fetchProductById("12");
+      if (result.status === 200) {
+        setError(null);
+        setProductDetails(result.data)
+      } else {
+        setError(result.error);
+        setProductDetails(null);
+      }
+      setLoading(false);
+    }
+
+    getData();
+
+  }, [])
 
   if (loading) return <Loading />
+  if (productDetails !== null) return (
+    <h1>{productDetails.sellerUsername}</h1>
+  )
   if (error !== null) return <Error code={error.code} message={error.message} />
 
-  return (
-    <h1>{productDetailsType.sellerUsername}</h1>
-  )
+  return <></>
+
 }
 
-export default ProductDetails
+export default ProductDetails;
 
 
---
+++ client/src/fake_data/productDetailsData.ts
@@ -0,0 +1,16 @@
+const productDetailsData = {
+    productId: 21,
+    productName: "Product name",
+    productPrice: 21.59,
+    productCategory: "Electronics",
+    productAttachements: ["link1", "link2"], // array of product attachments' names or links
+    sellerUsername: "Landscaping Inc",
+    // stats: [ int32, float32 ];
+    stockQuantity: 7,
+    productDescription: "Some Description",  //optional
+    productMaterial: "Plastic/metal", //optional
+    productWeight: 15.600, //optional
+    productWidth: 1400, //optional
+    productLenght: 10.00, //optional
+    productHeight: 7.00 //optional
+}
